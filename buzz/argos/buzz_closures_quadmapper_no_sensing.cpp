9ae858d65368987c38c67ede745471f80943fd7c
Pierre-Yves Lajoie <lajoie.py@gmail.com>
2019-04-25 18:31:48 +0000

    Refactoring of buzz closures in separate cpp files

---
+++ buzz/argos/buzz_closures_quadmapper_no_sensing.cpp
@@ -0,0 +1,134 @@
+#include "buzz_controller_quadmapper_no_sensing.h"
+
+namespace buzz_quadmapper {
+
+/****************************************/
+/************ Buzz Closures *************/
+/****************************************/
+
+static int BuzzComputeFakeRendezVousSeparator(buzzvm_t vm) {
+   /* Push the vector components */
+   buzzvm_lload(vm, 1);
+   buzzvm_lload(vm, 2);
+   buzzvm_lload(vm, 3);
+   buzzvm_lload(vm, 4);
+   buzzvm_lload(vm, 5);
+   buzzvm_lload(vm, 6);
+   buzzvm_lload(vm, 7);
+   /* Retrieve parameters and check their types */
+   buzzobj_t b_other_robot_pose_id = buzzvm_stack_at(vm, 7);
+   buzzobj_t b_gt_x = buzzvm_stack_at(vm, 6);
+   buzzobj_t b_gt_y = buzzvm_stack_at(vm, 5);
+   buzzobj_t b_gt_z = buzzvm_stack_at(vm, 4);
+   buzzobj_t b_gt_yaw = buzzvm_stack_at(vm, 3);
+   buzzobj_t b_other_robot_id = buzzvm_stack_at(vm, 2);
+   buzzobj_t b_this_robot_pose_id = buzzvm_stack_at(vm, 1);
+   CVector3 estimate_translation, gt_translation;
+   CQuaternion estimate_orientation, gt_orientation;
+   int other_robot_id, other_robot_pose_id, this_robot_pose_id;
+   if(b_this_robot_pose_id->o.type == BUZZTYPE_INT &&
+      b_other_robot_id->o.type == BUZZTYPE_INT &&
+      b_other_robot_pose_id->o.type == BUZZTYPE_INT &&
+      b_gt_x->o.type == BUZZTYPE_FLOAT &&
+      b_gt_y->o.type == BUZZTYPE_FLOAT &&
+      b_gt_z->o.type == BUZZTYPE_FLOAT &&
+      b_gt_yaw->o.type == BUZZTYPE_FLOAT) {
+
+      // Fill in ground truth
+      gt_translation.SetX(b_gt_x->f.value);
+      gt_translation.SetY(b_gt_y->f.value);
+      gt_translation.SetZ(b_gt_z->f.value);
+      CRadians gt_angle(b_gt_yaw->f.value);
+      CVector3 gt_axis(0, 0, 1);
+      gt_orientation = gt_orientation.FromAngleAxis(gt_angle, gt_axis);
+
+      // Get information for symbols
+      other_robot_id = b_other_robot_id->i.value;
+      other_robot_pose_id = b_other_robot_pose_id->i.value;
+      this_robot_pose_id = b_this_robot_pose_id->i.value;
+
+   } else {
+      buzzvm_seterror(vm,
+                      BUZZVM_ERROR_TYPE,
+                      "wrong parameter type for compute_fake_rendezvous_separator."
+         );
+      return vm->state;
+   }
+   /* Get pointer to the controller */
+   buzzvm_pushs(vm, buzzvm_string_register(vm, "controller", 1));
+   buzzvm_gload(vm);
+   /* Call function */
+   int is_outlier = reinterpret_cast<CBuzzControllerQuadMapperNoSensing*>(buzzvm_stack_at(vm, 1)->u.value)->ComputeNoisyFakeSeparatorMeasurement(gt_orientation, gt_translation, other_robot_pose_id, other_robot_id, this_robot_pose_id);
+   buzzvm_pushi(vm, is_outlier);
+
+   return buzzvm_ret1(vm);
+}
+
+/****************************************/
+/****************************************/
+
+static int BuzzMoveForwardFakeOdometry(buzzvm_t vm) {
+   /* Push the vector components */
+   buzzvm_lload(vm, 1);
+   buzzvm_lload(vm, 2);
+   /* Create a new vector with that */
+   CVector3 translation;
+   int simulation_time_divider;
+   buzzobj_t step = buzzvm_stack_at(vm, 2);
+   buzzobj_t b_simulation_time_divider = buzzvm_stack_at(vm, 1);
+   if(step->o.type == BUZZTYPE_INT) translation.SetX(step->i.value);
+   else if(step->o.type == BUZZTYPE_FLOAT) translation.SetX(step->f.value);
+   else {
+      buzzvm_seterror(vm,
+                      BUZZVM_ERROR_TYPE,
+                      "move_forward(x,y): expected %s, got %s in first argument",
+                      buzztype_desc[BUZZTYPE_FLOAT],
+                      buzztype_desc[step->o.type]
+         );
+      return vm->state;
+   }   
+   if(b_simulation_time_divider->o.type == BUZZTYPE_INT) simulation_time_divider = b_simulation_time_divider->i.value;
+   else {
+      buzzvm_seterror(vm,
+                      BUZZVM_ERROR_TYPE,
+                      "move_forward(x,y): expected %s, got %s in first argument",
+                      buzztype_desc[BUZZTYPE_INT],
+                      buzztype_desc[b_simulation_time_divider->o.type]
+         );
+      return vm->state;
+   }    
+   /* Get pointer to the controller */
+   buzzvm_pushs(vm, buzzvm_string_register(vm, "controller", 1));
+   buzzvm_gload(vm);
+   /* Call function */
+   int current_pose_id = reinterpret_cast<CBuzzControllerQuadMapperNoSensing*>(buzzvm_stack_at(vm, 1)->u.value)->MoveForwardFakeOdometry(translation, simulation_time_divider);
+   
+   buzzvm_pushi(vm, current_pose_id);
+
+   return buzzvm_ret1(vm);
+}
+
+/****************************************/
+/************ Registration **************/
+/****************************************/
+
+buzzvm_state CBuzzControllerQuadMapperNoSensing::RegisterFunctions() {
+   CBuzzControllerQuadMapper::RegisterFunctions();
+
+   /* Register mapping without sensing specific functions */
+   buzzvm_pushs(m_tBuzzVM, buzzvm_string_register(m_tBuzzVM, "compute_fake_rendezvous_separator", 1));
+   buzzvm_pushcc(m_tBuzzVM, buzzvm_function_register(m_tBuzzVM, BuzzComputeFakeRendezVousSeparator));
+   buzzvm_gstore(m_tBuzzVM);
+   buzzvm_pushs(m_tBuzzVM, buzzvm_string_register(m_tBuzzVM, "move_forward_fake_odometry", 1));
+   buzzvm_pushcc(m_tBuzzVM, buzzvm_function_register(m_tBuzzVM, BuzzMoveForwardFakeOdometry));
+   buzzvm_gstore(m_tBuzzVM);
+
+   return m_tBuzzVM->state;
+}
+
+/****************************************/
+/****************************************/
+
+REGISTER_CONTROLLER(CBuzzControllerQuadMapperNoSensing, "buzz_controller_quadmapper_no_sensing");
+
+}
