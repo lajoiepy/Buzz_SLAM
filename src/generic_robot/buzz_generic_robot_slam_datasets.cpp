3a56f7da9c741f7e258a7e0092c1305096102b22
Pierre-Yves Lajoie <lajoie.py@gmail.com>
2019-07-12 21:41:45 +0000

    Remove delay

--- src/generic_robot/buzz_generic_robot_slam_datasets.cpp
+++ src/generic_robot/buzz_generic_robot_slam_datasets.cpp
@@ -75,7 +75,7 @@ int main(int argc, char** argv) {
       while(!done && !buzz_script_done()){
          std::cout << "Buzz VM Step " << step << std::endl;
          buzz_script_step();
-         std::this_thread::sleep_for(std::chrono::milliseconds(100));
+         //std::this_thread::sleep_for(std::chrono::milliseconds(100));
          step++;
       }
       /* Cleanup */
